31fe40c709943239b5cdb0568324862dc59f78dc
Tristan Laclau <101633294+Tristan-Laclau@users.noreply.github.com>
2022-06-24 14:48:45 +0200

    perdu pour perdu

--- src/main/java/fr/fms/TpShopMvcApp.java
+++ src/main/java/fr/fms/TpShopMvcApp.java
@@ -29,41 +29,41 @@ public class TpShopMvcApp implements CommandLineRunner  {
 	}
 	
 	public void generateValues() {
-		Category smartphone = job.categoryRepository.save(new Category("Smartphone"));
-		Category pc = job.categoryRepository.save(new Category("Ordinateur"));
-		Category tablet = job.categoryRepository.save(new Category("Tablette"));
-		Category printer = job.categoryRepository.save(new Category("Imprimante"));
-		Category camera = job.categoryRepository.save(new Category("Camera"));
-		Category tv = job.categoryRepository.save(new Category("TV"));
+//		Category smartphone = job.categoryRepository.save(new Category("Smartphone"));
+//		Category pc = job.categoryRepository.save(new Category("Ordinateur"));
+//		Category tablet = job.categoryRepository.save(new Category("Tablette"));
+//		Category printer = job.categoryRepository.save(new Category("Imprimante"));
+//		Category camera = job.categoryRepository.save(new Category("Camera"));
+//		Category tv = job.categoryRepository.save(new Category("TV"));
 		
-		job.articleRepository.save(new Article("S8","Samsung",250,1,smartphone, "samsung.jpg"));
-		job.articleRepository.save(new Article("S9","Samsung", 300,1,smartphone, "samsung.jpg"));
-		job.articleRepository.save(new Article("iPhone 10","Apple",500,1,smartphone, "iphone.jpg"));		
-		job.articleRepository.save(new Article("MI11","Xiaomi",100,1,smartphone, "xiaomi.jpg"));
-		job.articleRepository.save(new Article("9 Pro","OnePlus",200,1,smartphone, "oneplus.jpg"));
-		job.articleRepository.save(new Article("Pixel 5","Google",350,1,smartphone, "googleSp.jpg"));
-		job.articleRepository.save(new Article("F3","Poco",150,1,smartphone, "poco.jpg"));
-		
-		job.articleRepository.save(new Article("810","Dell",550,1,pc, "dellpc.jpg"));
-		job.articleRepository.save(new Article("F756","Asus",600,1,pc, "asuspc.jpg"));
-		job.articleRepository.save(new Article("E80","Asus",700,1,pc, "asuspc.jpg"));
-		job.articleRepository.save(new Article("Pro","MacBook",1000,1,pc, "macbook.jpg"));
-		job.articleRepository.save(new Article("Air","MacBook",1200,1,pc, "macbook.jpg"));
-		
-		job.articleRepository.save(new Article("XL 5","IPad",300,1,tablet, "testimage1.png"));
-		job.articleRepository.save(new Article("XL 7","IPad",500,1,tablet, "testimage1.png"));
-		
-		
-		job.articleRepository.save(new Article("MG30","Canon",50,1,printer, "testimage1.png"));
-		job.articleRepository.save(new Article("MG50","Canon",60,1,printer, "testimage1.png"));
-		job.articleRepository.save(new Article("800","HP",50,1,printer, "testimage1.png"));
-		job.articleRepository.save(new Article("3T","Epson",100,1,printer, "testimage1.png"));
-		
-		job.articleRepository.save(new Article("7","GoPro",150,1,camera, "testimage1.png"));
-		job.articleRepository.save(new Article("10","GoPro",200,1,camera, "testimage1.png"));
-		
-		job.articleRepository.save(new Article("HT","Panasonic",1500,1,tv, "testimage1.png"));
-		job.articleRepository.save(new Article("L43","Philips",450,1,tv, "testimage1.png"));	
+//		job.articleRepository.save(new Article("S8","Samsung",250,1,smartphone, "samsung.jpg"));
+//		job.articleRepository.save(new Article("S9","Samsung", 300,1,smartphone, "samsung.jpg"));
+//		job.articleRepository.save(new Article("iPhone 10","Apple",500,1,smartphone, "iphone.jpg"));
+//		job.articleRepository.save(new Article("MI11","Xiaomi",100,1,smartphone, "xiaomi.jpg"));
+//		job.articleRepository.save(new Article("9 Pro","OnePlus",200,1,smartphone, "oneplus.jpg"));
+//		job.articleRepository.save(new Article("Pixel 5","Google",350,1,smartphone, "googleSp.jpg"));
+//		job.articleRepository.save(new Article("F3","Poco",150,1,smartphone, "poco.jpg"));
+//		
+//		job.articleRepository.save(new Article("810","Dell",550,1,pc, "dellpc.jpg"));
+//		job.articleRepository.save(new Article("F756","Asus",600,1,pc, "asuspc.jpg"));
+//		job.articleRepository.save(new Article("E80","Asus",700,1,pc, "asuspc.jpg"));
+//		job.articleRepository.save(new Article("Pro","MacBook",1000,1,pc, "macbook.jpg"));
+//		job.articleRepository.save(new Article("Air","MacBook",1200,1,pc, "macbook.jpg"));
+//		
+//		job.articleRepository.save(new Article("XL 5","IPad",300,1,tablet, "testimage1.png"));
+//		job.articleRepository.save(new Article("XL 7","IPad",500,1,tablet, "testimage1.png"));
+//		
+//		
+//		job.articleRepository.save(new Article("MG30","Canon",50,1,printer, "testimage1.png"));
+//		job.articleRepository.save(new Article("MG50","Canon",60,1,printer, "testimage1.png"));
+//		job.articleRepository.save(new Article("800","HP",50,1,printer, "testimage1.png"));
+//		job.articleRepository.save(new Article("3T","Epson",100,1,printer, "testimage1.png"));
+//		
+//		job.articleRepository.save(new Article("7","GoPro",150,1,camera, "testimage1.png"));
+//		job.articleRepository.save(new Article("10","GoPro",200,1,camera, "testimage1.png"));
+//		
+//		job.articleRepository.save(new Article("HT","Panasonic",1500,1,tv, "testimage1.png"));
+//		job.articleRepository.save(new Article("L43","Philips",450,1,tv, "testimage1.png"));	
 		
 		
 //		job.userRepository.save(new User(null,"Mathieu","Fix","Dax", "mathieu.fix@fms.com", 118218, "mf","ADMIN"));
